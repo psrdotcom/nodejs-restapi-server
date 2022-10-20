4e88cc7d4edfbe518c3df98b30ac03d083a8808c
Suresh Raju Pilli <sureshraju.pilli@gmail.com>
2022-10-20 11:13:21 +0530

    Updated from var to let and const
    Variables datatype update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 app.get('/', function(req, res){
     res.send('Hello PSR');
 })
 
-var server = app.listen(9090, function(){
-    var host = server.address().address
-    var port = server.address().port
+let server = app.listen(9090, function(){
+    const host = server.address().address
+    const port = server.address().port
 
     console.log("Simple server listening at http://%s:%s", host, port)
 })
